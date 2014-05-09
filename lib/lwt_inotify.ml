dcf93f0adafce98c1e55052a248565480d6005a1
Peter Zotov <whitequark@whitequark.org>
2014-05-09 11:19:46 +0400

    Use less horrible desugared syntax in place of try_lwt.

--- lib/lwt_inotify.ml
+++ lib/lwt_inotify.ml
@@ -7,30 +7,39 @@ type t = {
 }
 
 let create () =
-  catch (fun _ ->
+  try
     let unix_fd = Inotify.create () in
     return {
       queue   = Queue.create ();
       lwt_fd  = Lwt_unix.of_unix_file_descr unix_fd;
-      unix_fd; }) fail
+      unix_fd; }
+  with exn ->
+    Lwt.fail exn
 
 let add_watch inotify path selector =
-  catch (fun _ ->
-    return (Inotify.add_watch inotify.unix_fd path selector)) fail
+  try
+    return (Inotify.add_watch inotify.unix_fd path selector)
+  with exn ->
+    Lwt.fail exn
 
 let rm_watch inotify wd =
-  catch (fun _ ->
-    return (Inotify.rm_watch inotify.unix_fd wd)) fail
+  try
+    return (Inotify.rm_watch inotify.unix_fd wd)
+  with exn ->
+    Lwt.fail exn
 
 let rec read inotify =
   try
     return (Queue.take inotify.queue)
   with Queue.Empty ->
     Lwt_unix.wait_read inotify.lwt_fd >>= fun () ->
-    catch (fun _ ->
+    begin try
       let events = Inotify.read inotify.unix_fd in
       List.iter (fun event -> Queue.push event inotify.queue) events;
-      return_unit) fail >>= fun () ->
+      return_unit
+    with exn ->
+      Lwt.fail exn
+    end >>= fun () ->
     read inotify
 
 let rec try_read inotify =
