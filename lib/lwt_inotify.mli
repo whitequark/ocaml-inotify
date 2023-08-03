73cd64d75e12807a4ac1e130a4542580132b7436
Ivan Gotovchits <ivg@forallsecure.com>
2023-08-03 15:31:53 +0000

    adds pure operators
    Adds pure (non-lwt) `create'`, `add_watch'`, and `rm_watch'` operators.

--- lib/lwt_inotify.mli
+++ lib/lwt_inotify.mli
@@ -1,17 +1,27 @@
-(** An [Lwt] wrapper for {!Inotify} module *)
+(** An [Lwt] wrapper for {!Inotify} module.
+
+    Note, whenever it is possible we provide two version of an
+    operation:
+    - [f'] - a pure non-blocking operation;
+    - [f] - the same operation lifted into the Lwt monad.
+*)
 
 (** Type of inotify descriptors. *)
 type t
 
 (** [create ()] returns a new inotify descriptor. *)
 val create    : unit -> t Lwt.t
+val create'   : unit -> t
 
 (** [add_watch desc path events] sets up [desc] to watch for [events] occuring
     to [path], and returns a watch descriptor. *)
-val add_watch : t -> string -> Inotify.selector list -> Inotify.watch Lwt.t
+val add_watch  : t -> string -> Inotify.selector list -> Inotify.watch Lwt.t
+val add_watch' : t -> string -> Inotify.selector list -> Inotify.watch
+
 
 (** [rm_watch desc watch] stops [desc] from watching [watch]. *)
 val rm_watch  : t -> Inotify.watch -> unit Lwt.t
+val rm_watch' : t -> Inotify.watch -> unit
 
 (** [read desc] waits for an event to occur at [desc]. *)
 val read      : t -> Inotify.event Lwt.t
